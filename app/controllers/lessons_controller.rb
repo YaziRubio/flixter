bf352f0cf5307910d1141e5ec1cbf359b610811a
Yazmin Rubio <rubio.ys@gmail.com>
2018-01-04 22:10:40 +0000

    Adding Payment with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,8 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+  
+
   def show
   end
 
@@ -7,6 +11,13 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_current_course
+    ccourse = current_lesson.section.course
+    if current_user.enrolled_in?(ccourse) == false
+      redirect_to course_path(ccourse), alert: 'You must be enrolled in the course to view lessons'
+    end
   end
 
 end
